8010c9a8557f69a56eb8a03f6137bd256508ea56
khansagyna <amubafly11@gmail.com>
2026-02-09 02:59:01 +0700

    update responsive galeri

--- resources/views/user/galeri.blade.php
+++ resources/views/user/galeri.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+<style>
+    #galleryScroll::-webkit-scrollbar { display: none; }
+    #galleryScroll { 
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+</style>
+
 <section class="bg-white py-20">
     <div class="max-w-7xl mx-auto px-6 space-y-28">
 
@@ -38,26 +46,35 @@
                 </p>
             </div>
 
-            {{-- KANAN FOTO (BENTUK KHAS SS) --}}
-            <div class="md:col-span-9 space-y-4">
+{{-- KANAN FOTO --}}
+<div class="md:col-span-9 relative">
 
-                {{-- BARIS 1 --}}
-                <div class="grid grid-cols-5 gap-4">
-                    <img src="/images/galeri/{{ $item['folder'] }}/1.jpg" class="col-span-1 h-28 w-full object-cover rounded">
-                    <img src="/images/galeri/{{ $item['folder'] }}/2.jpg" class="col-span-1 h-28 w-full object-cover rounded">
-                    <img src="/images/galeri/{{ $item['folder'] }}/3.jpg" class="col-span-2 h-28 w-full object-cover rounded">
-                    <img src="/images/galeri/{{ $item['folder'] }}/4.jpg" class="col-span-1 h-28 w-full object-cover rounded">
-                </div>
+    <div id="galleryScroll"
+         class="overflow-x-auto scroll-smooth cursor-grab active:cursor-grabbing pb-6">
 
-                {{-- BARIS 2 --}}
-                <div class="grid grid-cols-5 gap-4">
-                    <img src="/images/galeri/{{ $item['folder'] }}/5.jpg" class="col-span-2 h-32 w-full object-cover rounded">
-                    <img src="/images/galeri/{{ $item['folder'] }}/6.jpg" class="col-span-1 h-32 w-full object-cover rounded">
-                    <img src="/images/galeri/{{ $item['folder'] }}/7.jpg" class="col-span-1 h-32 w-full object-cover rounded">
-                    <img src="/images/galeri/{{ $item['folder'] }}/8.jpg" class="col-span-1 h-32 w-full object-cover rounded">
-                </div>
+        <div class="min-w-[1200px] space-y-4 snap-x snap-mandatory">
 
+            {{-- BARIS 1 --}}
+            <div class="grid grid-cols-5 gap-4 snap-start">
+                <img src="/images/galeri/{{ $item['folder'] }}/1.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-1 h-28 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+                <img src="/images/galeri/{{ $item['folder'] }}/2.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-1 h-28 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+                <img src="/images/galeri/{{ $item['folder'] }}/3.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-2 h-28 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+                <img src="/images/galeri/{{ $item['folder'] }}/4.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-1 h-28 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
             </div>
+
+            {{-- BARIS 2 --}}
+            <div class="grid grid-cols-5 gap-4 snap-start">
+                <img src="/images/galeri/{{ $item['folder'] }}/5.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-2 h-32 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+                <img src="/images/galeri/{{ $item['folder'] }}/6.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-1 h-32 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+                <img src="/images/galeri/{{ $item['folder'] }}/7.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-1 h-32 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+                <img src="/images/galeri/{{ $item['folder'] }}/8.jpg" data-group="galeri-{{ $item['folder'] }}" class="gallery-img col-span-1 h-32 w-full object-cover rounded-lg cursor-pointer hover:scale-105 transition">
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
         </div>
         @endforeach
 
