233e01dde3cb77fe7e118ea407aa7e753f128aee
khansagyna <amubafly11@gmail.com>
2026-02-10 16:16:36 +0700

    update berita controller

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -8,15 +8,15 @@ class BeritaController extends Controller
 {
     private $beritaData = [
         [
-            'judul' => 'Open Recruitment LabMamen 2026',
+            'judul' => 'Open Recruitment LABMAMEN 2026',
             'slug' => 'praktikum-manajemen-2026',
             'gambar' => 'berita1.png',
             'tanggal' => '5 Februari 2026',
-            'excerpt' => 'Praktikum berjalan lancar dengan partisipasi aktif mahasiswa...',
+            'excerpt' => 'Mengapa Bergabung dengan Labmamen? Karena kami lebih dari sekadar komunitas. Kami adalah keluarga yang bertumbuh bersama, berbagi motivasi, dan meraih cita-cita tanpa pamrih. Labmamen adalah ruang untuk berkembang, berkontribusi, dan menemukan pengalaman yang tak terlupakan.',
             'isi' => 'Kegiatan praktikum tahun ini berjalan sangat baik. Mahasiswa mengikuti seluruh rangkaian kegiatan dengan antusias...'
         ],
         [
-            'judul' => 'Pelatihan Asisten Laboratorium',
+            'judul' => 'Training Asisten LABMAMEN',
             'slug' => 'pelatihan-asisten-lab',
             'gambar' => 'berita2.jpg',
             'tanggal' => '20 Januari 2026',
@@ -24,12 +24,12 @@ class BeritaController extends Controller
             'isi' => 'Pelatihan asisten laboratorium dilakukan selama dua hari penuh dengan berbagai materi teknis dan softskill...'
         ],
         [
-            'judul' => 'Workshop Manajemen Proyek',
-            'slug' => 'workshop-manajemen-proyek',
-            'gambar' => 'berita3.jpg',
+            'judul' => 'Buka Bersama LABMAMEN 2026',
+            'slug' => 'bukber labmamen',
+            'gambar' => 'berita3.jpeg',
             'tanggal' => '10 Januari 2026',
-            'excerpt' => 'Workshop ini menghadirkan pemateri dari industri profesional...',
-            'isi' => 'Workshop manajemen proyek memberikan wawasan baru kepada mahasiswa mengenai pengelolaan proyek secara nyata...'
+            'excerpt' => 'Lab Manajemen Menengah mengadakan kegiatan Buka Puasa Bersama sebagai momen kebersamaan untuk mempererat silaturahmi antara asisten, staf, dan alumni',
+            'isi' => 'WBuka Puasa Bersama (Bukber) Laboratorium Manajemen Menengah dilaksanakan pada 7 Maret 2026 pukul 16.00 dalam suasana hangat dan penuh kebersamaan. Kegiatan ini menjadi momen istimewa yang mempertemukan asisten, staf, dan mahasiswa untuk mempererat silaturahmi di luar aktivitas akademik. Acara diisi dengan ramah tamah, tausiyah singkat, serta berbuka dan makan bersama, sehingga menghadirkan kebersamaan yang sederhana namun penuh makna.'
         ],
     ];
 
